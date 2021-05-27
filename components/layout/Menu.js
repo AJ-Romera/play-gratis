ec3cdb79259fece3d5553c259e7b408d7837af2c
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-27 18:45:01 +0200

    CHNG: Make and Style the Menu

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -2,9 +2,26 @@ import Link from 'next/link';
 
 function Menu() {
 	return (
-		<div>
-			<p className='text-gray-200'>From Menu</p>
-		</div>
+		<nav className='pl-8'>
+			<Link href='/'>
+				<a className='text-xl font-normal text-gray-200 pl-8'>Home</a>
+			</Link>
+			<Link href='/explore'>
+				<a className='text-xl font-normal text-gray-200 pl-8'>
+					Explore
+				</a>
+			</Link>
+			<Link href='/popular'>
+				<a className='text-xl font-normal text-gray-200 pl-8'>
+					Popular
+				</a>
+			</Link>
+			<Link href='/newest'>
+				<a className='text-xl font-normal text-gray-200 pl-8 mr-0'>
+					Newest
+				</a>
+			</Link>
+		</nav>
 	);
 }
 
