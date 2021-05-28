1a2e30e0ec6242b3174a43875b706a6dad9e13a4
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-28 12:53:32 +0200

    CHNG: Create the Basic Pages of the Proyect

--- pages/explore.js
+++ pages/explore.js
@@ -1,15 +1,15 @@
 import Layout from '../components/layout/Layout';
 
-function Home() {
+function Explore() {
 	return (
 		<div>
-			<Layout title='PlayGratis F2P Games'>
+			<Layout title='Explore F2P Games'>
 				<p className='text-xl font-normal text-gray-200 bg-blue-700 pl-8'>
-					From home (/) page
+					From /explore page
 				</p>
 			</Layout>
 		</div>
 	);
 }
 
-export default Home;
+export default Explore;
--- pages/popular.js
+++ pages/popular.js
@@ -1,15 +1,15 @@
 import Layout from '../components/layout/Layout';
 
-function Home() {
+function Popular() {
 	return (
 		<div>
-			<Layout title='PlayGratis F2P Games'>
+			<Layout title='Popular F2P Games'>
 				<p className='text-xl font-normal text-gray-200 bg-blue-700 pl-8'>
-					From home (/) page
+					From /popular page
 				</p>
 			</Layout>
 		</div>
 	);
 }
 
-export default Home;
+export default Popular;
