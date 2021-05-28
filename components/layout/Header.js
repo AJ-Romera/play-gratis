0ea8edcb1102f03690be84ce2e7fabcebf6295ec
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-28 17:33:45 +0200

    FIX: Move the Menu to the Left in the Same Div as the Logo

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,16 +6,18 @@ function Header() {
 	return (
 		<header>
 			<div className='container mx-auto py-4 px-6 max-w-full flex justify-between items-center border-b-2 border-solid border-gray-600 bg-gray-800'>
-				<Link href='/'>
-					<a className='text-3xl font-semibold italic text-gray-200 mr-8 uppercase'>
-						Play
-						<span className='text-3xl font-thin italic text-gray-300 uppercase'>
-							gratis
-						</span>
-					</a>
-				</Link>
-
-				<Menu />
+				<div className='container flex items-center'>
+					>
+					<Link href='/'>
+						<a className='text-3xl font-semibold italic text-gray-200 uppercase'>
+							Play
+							<span className='text-3xl font-thin italic text-gray-300 uppercase'>
+								gratis
+							</span>
+						</a>
+					</Link>
+					<Menu />
+				</div>
 
 				<Search />
 			</div>
--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -2,22 +2,22 @@ import Link from 'next/link';
 
 function Menu() {
 	return (
-		<nav className='pl-8'>
+		<nav className='pl-10'>
 			<Link href='/'>
-				<a className='text-xl font-normal text-gray-200 pl-8'>Home</a>
+				<a className='text-xl font-normal text-gray-200'>Home</a>
 			</Link>
 			<Link href='/explore'>
-				<a className='text-xl font-normal text-gray-200 pl-8'>
+				<a className='text-xl font-normal text-gray-200 pl-7'>
 					Explore
 				</a>
 			</Link>
 			<Link href='/popular'>
-				<a className='text-xl font-normal text-gray-200 pl-8'>
+				<a className='text-xl font-normal text-gray-200 pl-7'>
 					Popular
 				</a>
 			</Link>
 			<Link href='/newest'>
-				<a className='text-xl font-normal text-gray-200 pl-8 mr-0'>
+				<a className='text-xl font-normal text-gray-200 pl-7 mr-0'>
 					Newest
 				</a>
 			</Link>
