6c817fe2ab66c5c537a63b364d0e859febd6dbb9
AJ-Romera <ajromeraalfonso@gmail.com>
2021-05-31 19:51:07 +0200

    CHNG: Style and Add Content to the Index

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,22 @@ function Home() {
 	return (
 		<div>
 			<Layout title='PlayGratis F2P Games'>
-				<p className='text-xl font-normal text-gray-200 bg-blue-700 pl-8'>
-					From home (/) page
-				</p>
+				<div className='flex flex-row items-center justify-center flex-1 mx-auto max-w-5xl text-center'>
+					<div className='grid grid-cols-2 gap-4'>
+						<div>
+							<p className='text-5xl font-bold text-gray-200'>
+								You can buy expensive games or you can find
+								amazing free-to-play games
+							</p>
+							<p className='text-3xl font-normal text-gray-200'>
+								Button here
+							</p>
+						</div>
+						<p className='text-xl font-normal text-gray-200 bg-blue-700'>
+							IMAGE HERE
+						</p>
+					</div>
+				</div>
 			</Layout>
 		</div>
 	);
