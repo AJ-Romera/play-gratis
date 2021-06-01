857cab9a32411e47b8cd40e220c6777bb5da5dbf
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-01 18:20:18 +0200

    CHNG: Fetch Games Data from an API in Explore

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button({ btnMessage }) {
 	return (
-		<button class='bg-gray-700 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-gray-100 rounded-2xl hover:shadow-lg hover:bg-gray-800'>
+		<button className='bg-gray-700 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-gray-100 rounded-2xl hover:shadow-lg hover:bg-gray-800'>
 			{btnMessage}
 		</button>
 	);
--- pages/explore.js
+++ pages/explore.js
@@ -1,15 +1,45 @@
+import Image from 'next/image';
 import Layout from '../components/layout/Layout';
 
-function Explore() {
+function Explore({ games }) {
 	return (
 		<div>
 			<Layout title='Explore F2P Games'>
-				<p className='text-xl font-normal text-gray-200 bg-blue-700 pl-8'>
-					From /explore page
-				</p>
+				<h1 className='text-5xl font-bold text-gray-200'>
+					List of Games:
+				</h1>
+				{console.log(games)}
+				<ul className='text-3xl font-semibold text-gray-200'>
+					{games.map((game) => (
+						<li key={game.id}>
+							<Image
+								src={game.thumbnail}
+								alt='sdgsdfgdsfg'
+								width={450}
+								height={300}
+							/>
+							<h2>{game.title}</h2>
+						</li>
+					))}
+				</ul>
 			</Layout>
 		</div>
 	);
+}
+
+export async function getStaticProps(context) {
+	const res = await fetch(`https://www.freetogame.com/api/games`);
+	const data = await res.json();
+
+	if (!data) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: { games: data }, // will be passed to the page component as props
+	};
 }
 
 export default Explore;
