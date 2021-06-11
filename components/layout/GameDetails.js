ae6d5a6cd45b9c402096daf3aa1bfa23c6902445
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-11 16:30:09 +0200

    CHNG: Add Two SVG Icons and Style them Properly

--- components/layout/GameDetails.js
+++ components/layout/GameDetails.js
@@ -47,9 +47,23 @@ function GameDetails({ game }) {
 				<p className='font-normal text-justify text-lg text-gray-200'>
 					Aditional Information
 				</p>
-				<p className='font-normal text-justify text-lg text-gray-200'>
-					{/* i info icon HERE */} Please note this free-to-play game
-					may or may not offer optional in-game purchases.
+				<p className='flex flex-row font-normal text-justify text-lg text-gray-200'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						className='h-5 w-5'
+						viewBox='0 0 20 20'
+						fill='currentColor'
+					>
+						<path
+							fillRule='evenodd'
+							d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
+							clipRule='evenodd'
+						/>
+					</svg>
+					<span className='ml-2'>
+						Please note this free-to-play game may or may not offer
+						optional in-game purchases.
+					</span>
 				</p>
 				<div className='grid grid-cols-2 gap-4'>
 					<div className='flex flex-col bg-white'>
@@ -74,9 +88,24 @@ function GameDetails({ game }) {
 					</div>
 					<div className='flex flex-col bg-white'>
 						<span>Platform</span>
-						<span>
-							{/* ICONO DE WINDOWS AQUÍ */}
-							{game.platform} (Client)
+						<span className='flex flex-row'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='h-5 w-5'
+								fill='none'
+								viewBox='0 0 24 24'
+								stroke='currentColor'
+							>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									strokeWidth='2'
+									d='M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
+								/>
+							</svg>
+							<span className='ml-2'>
+								{game.platform} (Client)
+							</span>
 						</span>
 					</div>
 				</div>
