49e8945ac44830b0cf85d51fa87dc668da6b893d
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-15 17:58:17 +0200

    CHNG: Renders Components Conditionally

--- components/layout/MainSection.js
+++ components/layout/MainSection.js
@@ -9,10 +9,12 @@ function MainSection({ game }) {
 			<h1 className='font-medium text-4xl text-justify text-indigo-100 mb-2'>
 				{game.title}
 			</h1>
-			<About game={game} />
+			{game.description && <About game={game} />}
 			<AditionalInfo game={game} />
-			<Screenshots game={game} />
-			<MinSystemRequirements game={game} />
+			{game.screenshots && <Screenshots game={game} />}
+			{game.minimum_system_requirements && (
+				<MinSystemRequirements game={game} />
+			)}
 			<hr className='mb-3 border-t-1 border-solid border-gray-600' />
 			<p className='font-normal text-justify text-sm text-gray-400'>
 				All material on this page is copyrighted by{' '}
