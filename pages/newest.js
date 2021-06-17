d0115699e8a72b0662874f8d03552627cd82d0c6
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-17 14:18:24 +0200

    CHNG: Make Entire Newest Page

--- pages/newest.js
+++ pages/newest.js
@@ -1,11 +1,30 @@
-function Newest() {
+import Card from '../components/ui/Card';
+
+function Newest({ newestGames }) {
 	return (
-		<div>
-			<p className='text-xl font-normal text-gray-200 bg-blue-700 my-10'>
-				From /newest page
-			</p>
+		<div className='my-10 sm:grid gap-4 md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center'>
+			{newestGames.map((game) => (
+				<Card key={game.id} game={game} />
+			))}
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const res = await fetch(
+		`https://www.freetogame.com/api/games?sort-by=release-date`
+	);
+	const data = await res.json();
+
+	if (!data) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: { newestGames: data },
+	};
 }
 
 export default Newest;
