fec15eaa7e1a2fac6041a13c2063b868ea88b1fd
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-18 19:12:24 +0200

    CHNG: Make Pagination Responsive Mobile Prev and Next Buttons

--- components/ui/Pagination.js
+++ components/ui/Pagination.js
@@ -7,20 +7,45 @@ function Pagination({ gamesPerPage, totalGames, paginate, currentPage }) {
 
 	return (
 		<div className='bg-gray-800 px-4 py-3 flex items-center justify-between mb-4 border-t border-gray-200 rounded sm:px-6'>
+			{/* Responsive Prev and Next "buttons" for mobile */}
 			<div className='flex-1 flex justify-between sm:hidden'>
-				<a
-					href='#'
-					className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-				>
-					Previous
-				</a>
-				<a
-					href='#'
-					className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-				>
-					Next
-				</a>
+				{currentPage < 2 ? (
+					<a
+						href='#'
+						className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-200 opacity-50 pointer-events-none'
+					>
+						<span className=''>Previous</span>
+					</a>
+				) : (
+					<a
+						href='#'
+						className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-200'
+						onClick={() => paginate(currentPage - 1)}
+					>
+						<span className=''>Previous</span>
+					</a>
+				)}
+				<span className='bg-indigo-100 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-bold rounded-md pointer-events-none'>
+					{currentPage}
+				</span>
+				{currentPage > pageNumbers.length - 1 ? (
+					<a
+						href='#'
+						className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-200 opacity-50 pointer-events-none'
+					>
+						<span className=''>Next</span>
+					</a>
+				) : (
+					<a
+						href='#'
+						className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-200'
+						onClick={() => paginate(currentPage + 1)}
+					>
+						<span className=''>Next</span>
+					</a>
+				)}
 			</div>
+			{/* Prev and Next "buttons" for bigger screens / desktop */}
 			<div className='hidden sm:flex-1 sm:flex sm:items-center sm:justify-between'>
 				<div>
 					<p className='text-sm text-gray-700'>
@@ -38,7 +63,6 @@ function Pagination({ gamesPerPage, totalGames, paginate, currentPage }) {
 							<a
 								href='#'
 								className='relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 opacity-50 pointer-events-none'
-								onClick={() => paginate(currentPage - 1)}
 							>
 								<span className='sr-only'>Previous</span>
 								<svg
@@ -120,7 +144,6 @@ function Pagination({ gamesPerPage, totalGames, paginate, currentPage }) {
 							<a
 								href='#'
 								className='relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 opacity-50 pointer-events-none'
-								onClick={() => paginate(currentPage + 1)}
 							>
 								<span className='sr-only'>Next</span>
 								<svg
