e748f6349db1015e7682d386490db47ad842c5cb
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-21 19:06:26 +0200

    CHNG: Make the Pagination of the Search Results

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import Layout from '../components/layout/Layout';
 import Card from '../components/ui/Card';
+import Pagination from '../components/ui/Pagination';
 
 function Search({ games }) {
 	const router = useRouter();
@@ -10,6 +10,17 @@ function Search({ games }) {
 	} = router;
 
 	const [result, setResult] = useState([]);
+
+	const [currentPage, setCurrentPage] = useState(1);
+	const [gamesPerPage] = useState(24);
+
+	// Get current games
+	const indexOfLastGame = currentPage * gamesPerPage;
+	const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+	const currentGames = result.slice(indexOfFirstGame, indexOfLastGame);
+
+	// Change page
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
 	useEffect(() => {
 		const wordsToSearch = q.toLowerCase();
@@ -24,10 +35,20 @@ function Search({ games }) {
 	}, [q, games]);
 
 	return (
-		<div className='my-10 sm:grid gap-4 md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center'>
-			{result.map((game) => (
-				<Card key={game.id} game={game} />
-			))}
+		<div>
+			<div className='my-10 sm:grid gap-4 md:grid-cols-2 xl:grid-cols-4 3xl:flex flex-wrap justify-center'>
+				{currentGames.map((game) => (
+					<Card key={game.id} game={game} />
+				))}
+			</div>
+			<Pagination
+				gamesPerPage={gamesPerPage}
+				totalGames={result.length}
+				paginate={paginate}
+				currentPage={currentPage}
+				indexOfFirstGame={indexOfFirstGame}
+				currentGames={currentGames}
+			/>
 		</div>
 	);
 }
