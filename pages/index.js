354661a4d1a206144a40e72bef4b931debf603c9
AJ-Romera <ajromeraalfonso@gmail.com>
2021-06-23 17:14:20 +0200

    CHNG: Make the Responsive of the Homepage

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ function Home() {
 		<div className='bg-clouds-patter bg-fixed bg-no-repeat bg-cover'>
 			<div className='bg-gray-900 bg-opacity-70'>
 				<div className='flex flex-row flex-1 items-center justify-center mx-auto max-w-5xl min-h-screen text-center'>
-					<div className='grid grid-cols-2 gap-4 items-center'>
+					<div className='flex flex-col sm:grid sm:grid-cols-2 sm:gap-4 sm:items-center'>
 						<section className='text-gray-600 body-font'>
 							<div className='container mx-auto flex px-3 py-24 md:flex-row flex-col items-center'>
-								<div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
-									<h1 className='title-font sm:text-xl text-5xl mb-10 font-bold text-gray-200'>
+								<div className='flex flex-col justify-center items-center text-center'>
+									<h1 className='mb-10 font-bold text-gray-200 text-4xl sm:text-3xl md:text-5xl'>
 										You can buy expensive games or you can
 										find amazing free-to-play games
 									</h1>
@@ -24,7 +24,7 @@ function Home() {
 								</div>
 							</div>
 						</section>
-						<div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
+						<div className='hidden sm:block'>
 							<Image
 								src='https://cdn.pixabay.com/photo/2016/10/30/23/05/controller-1784573_960_720.png'
 								alt='White Game Controller in a Blue Circle'
